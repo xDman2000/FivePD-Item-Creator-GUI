89af2539d5837c2d786110f7200e7dd64667d235
xDman2000 <43915301+xDman2000@users.noreply.github.com>
2020-05-03 11:32:21 +0200

    Added ItemOccurance for upcoming FivePD items.json change.

--- ItemCreator/ItemCreator.Designer.cs
+++ ItemCreator/ItemCreator.Designer.cs
@@ -28,22 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.illegal = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.multiplier = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btn_AddItem = new System.Windows.Forms.Button();
             this.tb_Name = new System.Windows.Forms.TextBox();
             this.lbl_Name = new System.Windows.Forms.Label();
-            this.rb_True = new System.Windows.Forms.RadioButton();
-            this.rb_False = new System.Windows.Forms.RadioButton();
+            this.rb_IllegalTrue = new System.Windows.Forms.RadioButton();
+            this.rb_IllegalFalse = new System.Windows.Forms.RadioButton();
             this.lbl_Multiplier = new System.Windows.Forms.Label();
             this.num_Multiplier = new System.Windows.Forms.NumericUpDown();
-            this.gb_Illegal = new System.Windows.Forms.GroupBox();
+            this.gb_IsIllegal = new System.Windows.Forms.GroupBox();
+            this.cb_Occurance = new System.Windows.Forms.ComboBox();
+            this.lbl_Occurance = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.illegal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.occurance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.multiplier = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.num_Multiplier)).BeginInit();
-            this.gb_Illegal.SuspendLayout();
+            this.gb_IsIllegal.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -52,44 +55,25 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.name,
             this.illegal,
+            this.occurance,
             this.multiplier});
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.Location = new System.Drawing.Point(170, 0);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(180, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.dataGridView1.Size = new System.Drawing.Size(281, 450);
+            this.dataGridView1.Size = new System.Drawing.Size(381, 450);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // name
-            // 
-            this.name.HeaderText = "Name";
-            this.name.Name = "name";
-            this.name.ReadOnly = true;
-            // 
-            // illegal
-            // 
-            this.illegal.HeaderText = "Illegal?";
-            this.illegal.Name = "illegal";
-            this.illegal.ReadOnly = true;
-            this.illegal.Width = 60;
-            // 
-            // multiplier
-            // 
-            this.multiplier.HeaderText = "Multiplier";
-            this.multiplier.Name = "multiplier";
-            this.multiplier.ReadOnly = true;
-            this.multiplier.Width = 60;
             // 
             // btn_AddItem
             // 
-            this.btn_AddItem.Location = new System.Drawing.Point(15, 132);
+            this.btn_AddItem.Location = new System.Drawing.Point(15, 182);
             this.btn_AddItem.Name = "btn_AddItem";
             this.btn_AddItem.Size = new System.Drawing.Size(141, 23);
             this.btn_AddItem.TabIndex = 1;
@@ -113,27 +97,27 @@
             this.lbl_Name.TabIndex = 3;
             this.lbl_Name.Text = "Name:";
             // 
-            // rb_True
+            // rb_IllegalTrue
             // 
-            this.rb_True.AutoSize = true;
-            this.rb_True.Checked = true;
-            this.rb_True.Location = new System.Drawing.Point(6, 16);
-            this.rb_True.Name = "rb_True";
-            this.rb_True.Size = new System.Drawing.Size(47, 17);
-            this.rb_True.TabIndex = 4;
-            this.rb_True.TabStop = true;
-            this.rb_True.Text = "True";
-            this.rb_True.UseVisualStyleBackColor = true;
+            this.rb_IllegalTrue.AutoSize = true;
+            this.rb_IllegalTrue.Checked = true;
+            this.rb_IllegalTrue.Location = new System.Drawing.Point(6, 16);
+            this.rb_IllegalTrue.Name = "rb_IllegalTrue";
+            this.rb_IllegalTrue.Size = new System.Drawing.Size(47, 17);
+            this.rb_IllegalTrue.TabIndex = 4;
+            this.rb_IllegalTrue.TabStop = true;
+            this.rb_IllegalTrue.Text = "True";
+            this.rb_IllegalTrue.UseVisualStyleBackColor = true;
             // 
-            // rb_False
+            // rb_IllegalFalse
             // 
-            this.rb_False.AutoSize = true;
-            this.rb_False.Location = new System.Drawing.Point(6, 39);
-            this.rb_False.Name = "rb_False";
-            this.rb_False.Size = new System.Drawing.Size(50, 17);
-            this.rb_False.TabIndex = 5;
-            this.rb_False.Text = "False";
-            this.rb_False.UseVisualStyleBackColor = true;
+            this.rb_IllegalFalse.AutoSize = true;
+            this.rb_IllegalFalse.Location = new System.Drawing.Point(6, 39);
+            this.rb_IllegalFalse.Name = "rb_IllegalFalse";
+            this.rb_IllegalFalse.Size = new System.Drawing.Size(50, 17);
+            this.rb_IllegalFalse.TabIndex = 5;
+            this.rb_IllegalFalse.Text = "False";
+            this.rb_IllegalFalse.UseVisualStyleBackColor = true;
             // 
             // lbl_Multiplier
             // 
@@ -166,23 +150,68 @@
             0,
             0});
             // 
-            // gb_Illegal
+            // gb_IsIllegal
             // 
-            this.gb_Illegal.Controls.Add(this.rb_False);
-            this.gb_Illegal.Controls.Add(this.rb_True);
-            this.gb_Illegal.Location = new System.Drawing.Point(15, 39);
-            this.gb_Illegal.Name = "gb_Illegal";
-            this.gb_Illegal.Size = new System.Drawing.Size(62, 61);
-            this.gb_Illegal.TabIndex = 10;
-            this.gb_Illegal.TabStop = false;
-            this.gb_Illegal.Text = "Illegal?";
+            this.gb_IsIllegal.Controls.Add(this.rb_IllegalFalse);
+            this.gb_IsIllegal.Controls.Add(this.rb_IllegalTrue);
+            this.gb_IsIllegal.Location = new System.Drawing.Point(15, 39);
+            this.gb_IsIllegal.Name = "gb_IsIllegal";
+            this.gb_IsIllegal.Size = new System.Drawing.Size(62, 61);
+            this.gb_IsIllegal.TabIndex = 10;
+            this.gb_IsIllegal.TabStop = false;
+            this.gb_IsIllegal.Text = "Illegal?";
+            // 
+            // cb_Occurance
+            // 
+            this.cb_Occurance.FormattingEnabled = true;
+            this.cb_Occurance.Location = new System.Drawing.Point(15, 155);
+            this.cb_Occurance.Name = "cb_Occurance";
+            this.cb_Occurance.Size = new System.Drawing.Size(141, 21);
+            this.cb_Occurance.TabIndex = 11;
+            // 
+            // lbl_Occurance
+            // 
+            this.lbl_Occurance.AutoSize = true;
+            this.lbl_Occurance.Location = new System.Drawing.Point(12, 139);
+            this.lbl_Occurance.Name = "lbl_Occurance";
+            this.lbl_Occurance.Size = new System.Drawing.Size(156, 13);
+            this.lbl_Occurance.TabIndex = 12;
+            this.lbl_Occurance.Text = "Where should this item appear?";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // illegal
+            // 
+            this.illegal.HeaderText = "Illegal?";
+            this.illegal.Name = "illegal";
+            this.illegal.ReadOnly = true;
+            this.illegal.Width = 60;
+            // 
+            // occurance
+            // 
+            this.occurance.HeaderText = "Occurance";
+            this.occurance.Name = "occurance";
+            this.occurance.ReadOnly = true;
+            // 
+            // multiplier
+            // 
+            this.multiplier.HeaderText = "Multiplier";
+            this.multiplier.Name = "multiplier";
+            this.multiplier.ReadOnly = true;
+            this.multiplier.Width = 60;
             // 
             // ItemCreator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(451, 450);
-            this.Controls.Add(this.gb_Illegal);
+            this.ClientSize = new System.Drawing.Size(561, 450);
+            this.Controls.Add(this.lbl_Occurance);
+            this.Controls.Add(this.cb_Occurance);
+            this.Controls.Add(this.gb_IsIllegal);
             this.Controls.Add(this.num_Multiplier);
             this.Controls.Add(this.lbl_Multiplier);
             this.Controls.Add(this.lbl_Name);
@@ -194,8 +223,8 @@
             this.Load += new System.EventHandler(this.ItemCreator_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.num_Multiplier)).EndInit();
-            this.gb_Illegal.ResumeLayout(false);
-            this.gb_Illegal.PerformLayout();
+            this.gb_IsIllegal.ResumeLayout(false);
+            this.gb_IsIllegal.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -204,17 +233,20 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn illegal;
-        private System.Windows.Forms.DataGridViewTextBoxColumn multiplier;
         private System.Windows.Forms.Button btn_AddItem;
         private System.Windows.Forms.TextBox tb_Name;
         private System.Windows.Forms.Label lbl_Name;
-        private System.Windows.Forms.RadioButton rb_True;
-        private System.Windows.Forms.RadioButton rb_False;
+        private System.Windows.Forms.RadioButton rb_IllegalTrue;
+        private System.Windows.Forms.RadioButton rb_IllegalFalse;
         private System.Windows.Forms.Label lbl_Multiplier;
         private System.Windows.Forms.NumericUpDown num_Multiplier;
-        private System.Windows.Forms.GroupBox gb_Illegal;
+        private System.Windows.Forms.GroupBox gb_IsIllegal;
+        private System.Windows.Forms.ComboBox cb_Occurance;
+        private System.Windows.Forms.Label lbl_Occurance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn illegal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn occurance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn multiplier;
     }
 }
 
--- ItemCreator/ItemCreator.cs
+++ ItemCreator/ItemCreator.cs
@@ -15,9 +15,18 @@ namespace ItemCreator
 {
     public partial class ItemCreator : Form
     {
+        private enum Occurance
+        {
+            Vehicles = 0,
+            Peds = 1,
+            Both = 2
+        }
+
         public ItemCreator()
         {
             InitializeComponent();
+
+            cb_Occurance.DataSource = Enum.GetValues(typeof(Occurance));
         }
 
         JArray items = new JArray();
@@ -30,7 +39,7 @@ namespace ItemCreator
                 items = JArray.Parse(File.ReadAllText(path));
                 foreach (JToken item in items)
                 {
-                    dataGridView1.Rows.Add(item["name"], item["isIllegal"], item["multiplier"]);
+                    dataGridView1.Rows.Add(item["name"], item["isIllegal"], (Occurance)Convert.ToInt32(item["itemLocation"]), item["multiplier"]);
                 }
             }
         }
@@ -42,28 +51,34 @@ namespace ItemCreator
             else
             {
                 string name = tb_Name.Text;
+
                 bool isIllegal = true;
-                if (rb_True.Checked)
+                if (rb_IllegalTrue.Checked)
                 { isIllegal = true; }
-                else if (rb_False.Checked)
+                else if (rb_IllegalFalse.Checked)
                 { isIllegal = false; }
+
+                Occurance enumOccurance = (Occurance)cb_Occurance.SelectedValue;
+                int occurance = (int)enumOccurance;
+
                 int multiplier = Convert.ToInt32(num_Multiplier.Value);
 
-                AddItemJson(name, isIllegal, multiplier);
-                AddItemGridView(name, isIllegal, multiplier);
+                AddItemJson(name, isIllegal, occurance, multiplier);
+                AddItemGridView(name, isIllegal, occurance, multiplier);
             }
         }
 
-        private void AddItemJson(string name, bool isIllegal, int multiplier)
+        private void AddItemJson(string name, bool isIllegal, int occurance, int multiplier)
         {
             JObject tempObject = new JObject();
             tempObject.Add("name", name);
             tempObject.Add("isIllegal", isIllegal);
+            tempObject.Add("itemLocation", occurance);
             tempObject.Add("multiplier", multiplier);
             items.Add(tempObject);
             File.WriteAllText(path, items.ToString());
         }
 
-        private void AddItemGridView(string name, bool isIllegal, int multiplier) => dataGridView1.Rows.Add(name, isIllegal, multiplier);
+        private void AddItemGridView(string name, bool isIllegal, int occurance, int multiplier) => dataGridView1.Rows.Add(name, isIllegal, (Occurance)occurance, multiplier);
     }
 }
